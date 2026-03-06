5dabbc53e755ab2ccd806dc892b3763e9f52b25d
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 17:02:14 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PASTEL GHOST';
+export let nameSongs = [
+	"DARK BEACH", /*1*/
+	"IRIS", /*2*/
+	"SILHOUETTE", /*3*/
+	"SHADOWS", /*4*/
+	"SKELETON", /*5*/
+	"PRISM", /*6*/
+	"PULSE", /*7*/
+	"AMETHYST", /*8*/
+	"3NDL3SS", /*9*/
+	"SAKURA", /*10*/
+	"TEARS", /*11*/
+	"POSSESSION", /*12*/
+	"EMOTION", /*13*/
+	"ABYSS", /*14*/
+	"LILACS", /*15*/
+	"ETHEREALITY", /*16*/
+	"UNDERWATER", /*17*/
+	"TRANSLUCID", /*18*/
+	"MERCURY", /*19*/
+	"EMBRACE", /*20*/
+]
